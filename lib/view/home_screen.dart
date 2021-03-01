4bbee3e18d51213caf0d7d620d596211d73722f4
RYgithub1 <ryo.rec.otani@gmail.com>
2021-03-01 22:56:44 +0900

    Add scaffold

---
+++ lib/view/home_screen.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+import 'postit_screen.dart';
+
+
+
+
+class HomeScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("goooooo"),
+        centerTitle: true,
+        actions: <Widget>[
+          IconButton(
+            icon: Icon(Icons.arrow_forward),
+            onPressed: () => Navigator.push(context, MaterialPageRoute(
+              builder: (_) => PostitScreen(),
+            )),
+          ),
+        ],
+      ),
+      body: Center(child: CircularProgressIndicator()),
+    );
+  }
+}
---
+++ lib/view/postit_screen.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+
+class PostitScreen extends StatefulWidget {
+  PostitScreen({Key key}) : super(key: key);
+
+  @override
+  _PostitScreenState createState() => _PostitScreenState();
+}
+
+class _PostitScreenState extends State<PostitScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+    );
+  }
+}
