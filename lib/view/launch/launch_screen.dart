6ed90a381d3de0ae70838389be123bb3737be5e0
RYgithub1 <ryo.rec.otani@gmail.com>
2021-03-05 08:19:17 +0900

    Add SwitchListTile

--- lib/view/launch/launch_screen.dart
+++ lib/view/launch/launch_screen.dart
@@ -4,10 +4,24 @@ import 'package:url_launcher/url_launcher.dart';
 
 
 
-class LaunchScreen extends StatelessWidget {
+class LaunchScreen extends StatefulWidget {
+  @override
+  _LaunchScreenState createState() => _LaunchScreenState();
+}
+
+
+
+
+class _LaunchScreenState extends State<LaunchScreen> {
+  bool _switchListTileToggled = false;   /// [Need rebuild -> Outside of build()]
+  String _switchTitle = 'Bulgaria: ';  /// [Need rebuild -> Outside of build()]
+
+
   @override
   Widget build(BuildContext context) {
     const _urlLaunch = 'https://flutter.dev/docs';
+    /// bool _switchListTileToggled = false;   /// [Need rebuild -> Outside of build()]
+    /// String _switchTitle = 'Bulgaria: ';  /// [Need rebuild -> Outside of build()]
 
     return Scaffold(
       appBar: AppBar(
@@ -29,6 +43,8 @@ class LaunchScreen extends StatelessWidget {
             mainAxisAlignment: MainAxisAlignment.center,
             crossAxisAlignment: CrossAxisAlignment.stretch,
             children: <Widget>[
+
+              /// [------ Launcher ------]
               RaisedButton(
                 child: Text('Flutter HP'),
                 // onPressed: () => {await launch('https://flutter.dev/docs')},
@@ -54,6 +70,32 @@ class LaunchScreen extends StatelessWidget {
               RaisedButton(
                 child: Text('Flutter MAIL'),
                 onPressed: () => {launch('mailto:flutterexample@gmail.com')},
+              ),
+              SizedBox(height: 20),
+
+              /// [------ SwitchListTile ------]
+              SwitchListTile(
+                value: _switchListTileToggled,
+                onChanged: (bool value) {
+                  setState(() {
+                    _switchListTileToggled = value;
+                    _switchTitle = 'Bulgaria -> $_switchListTileToggled';
+                  });
+                },
+                title: Text('Yoghurt'),
+                subtitle: Text(_switchTitle),
+                secondary:  Icon(
+                  Icons.thumb_up,
+                  color: _switchListTileToggled
+                      ? Colors.pink
+                      : Colors.green,
+                  size: 40.0,
+                ),
+                controlAffinity: ListTileControlAffinity.leading,
+                activeColor: Colors.orange,
+                activeTrackColor: Colors.red,
+                inactiveThumbColor: Colors.blue,
+                inactiveTrackColor: Colors.grey,
               ),
             ]
           ),
